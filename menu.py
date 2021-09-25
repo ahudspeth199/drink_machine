3b41b5ad58ac669f778624fc4135ceb16c1ad78e
Anthony Hudspeth <88898585+hudspeth@users.noreply.github.com>
2021-09-24 20:49:33 -0500

    Add files via upload

---
+++ menu.py
@@ -0,0 +1,26 @@
+OPTIONS = {
+    "soda": {
+        "type": {
+            "red", "green", "purple",
+        },
+        "cost": 1.5,
+    },
+
+
+    "energy drink": {
+        "type": {
+            "intense", "fizz", "hype",
+        },
+        "cost": 2.5,
+    },
+
+    "water": {
+        "type": {
+        "ocean", "spring", "mountain",
+        },
+        "cost": 1.0,
+    }
+
+
+}
+
